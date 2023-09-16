1252d992211460903a68da6c85098822adbaba24
staf3333 <staf3333@gmail.com>
2023-09-15 22:26:58 -0700

    add React template

---
+++ dash-client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>
+        OK bruh I'm going to try to make an example dashboard using Plotly Dash
+      </h1>
+    </>
+  );
+}
+
+export default App;
